a82338e58c3c6dab7275f1e0c40d4cc744a071a2
Mohamed Elhorafi <96639132+ElhorafiMohamed@users.noreply.github.com>
2024-03-27 15:54:53 +0000

    push authentification

--- src/main/java/com/example/elhorafi/security/SecurityConfig.java
+++ src/main/java/com/example/elhorafi/security/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.example.elhorafi.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
+    @Override
+    protected void configure (HttpSecurity http){
+http.csrf().disable();
+    }
 
 }
